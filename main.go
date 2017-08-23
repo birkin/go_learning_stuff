9c352c767442780c55fa95b4c0d811cb606885a1
birkin <birkin.diana@gmail.com>
2017-08-22 23:46:32 -0400

    var and function experimentation.

--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
   "fmt"
-  "os"
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    os.Exit(1)
-  }
-  fmt.Println("It's over", os.Args[1])
+
+    first, last := "birkin", "diana"
+    var middle string = "james"
+    fmt.Printf( "person, ```%s %s %s```\n", first, middle, last )
+
+    var sum int = add( 2, 3 )
+    fmt.Printf( "sum, ```%d```\n", sum )
+
+    var sum_validity bool = false
+    var returned_sum int
+    sum_validity, returned_sum = add_and_validate( 3, 4 )
+    fmt.Printf( "validity, ```%t```; sum, ```%d```\n", sum_validity, returned_sum )
+
 }
+
+func add( a int, b int ) int {
+    return a + b
+}
+
+func add_and_validate( a int, b int ) ( bool, int ) {
+    return true, a + b
+}
+
+
+// func main() {
+//   power := getPower()
+//   fmt.Printf("It's over %d\n", power)
+// }
+
+// func getPower() int {
+//   return 9001
+// }
