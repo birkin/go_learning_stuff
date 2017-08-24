1bf84e16920f8e10a02b6f80f2bc8ecb890c1e6b
birkin <birkin.diana@gmail.com>
2017-08-24 02:43:59 -0400

    experiments with structs.

--- main.go
+++ main.go
@@ -18,12 +18,38 @@ func main() {
     sum_validity, returned_sum = add_and_validate( 3, 4 )
     fmt.Printf( "validity, ```%t```; sum, ```%d```\n", sum_validity, returned_sum )
 
+    setup_saiyans()
+
 }
+
 
 func add( a int, b int ) int {
     return a + b
 }
 
+
 func add_and_validate( a int, b int ) ( bool, int ) {
     return true, a + b
 }
+
+
+func setup_saiyans() {
+    // define the struct
+    type Saiyan struct {
+        Name string
+        Power int
+    }
+    // create an instance
+    var goku Saiyan
+    goku.Name = "Goku"
+    goku.Power = 9000
+    // create another instance
+    birkin := Saiyan{
+        Name: "Birkin",
+        Power: 5000,
+    }
+    fmt.Printf( "goku power, ```%d```\n", goku.Power )
+    fmt.Printf( "birkin power, ```%d```\n", birkin.Power )
+}
+
+
