e6d97328a7a91d1b9822e33a5fb3bed8a16a5a11
birkin <birkin.diana@gmail.com>
2017-08-31 22:53:39 -0400

    updates comment-breaks.

--- main.go
+++ main.go
@@ -1,146 +1,141 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
 
-    first, last := "birkin", "diana"
-    var middle string = "james"
-    fmt.Printf( "person, ```%s %s %s```\n", first, middle, last )
+	first, last := "birkin", "diana"
+	var middle string = "james"
+	fmt.Printf("person, ```%s %s %s```\n", first, middle, last)
 
-    var sum int = add( 2, 3 )
-    fmt.Printf( "sum, ```%d```\n", sum )
+	var sum int = add(2, 3)
+	fmt.Printf("sum, ```%d```\n", sum)
 
-    var sum_validity bool = false
-    var returned_sum int
-    sum_validity, returned_sum = add_and_validate( 3, 4 )
-    fmt.Printf( "validity, ```%t```; sum, ```%d```\n", sum_validity, returned_sum )
+	var sum_validity bool = false
+	var returned_sum int
+	sum_validity, returned_sum = add_and_validate(3, 4)
+	fmt.Printf("validity, ```%t```; sum, ```%d```\n", sum_validity, returned_sum)
 
-    setup_saiyans()
+	setup_saiyans()
 
-    // copy/pointer experimentation
-    print_as_copy()
-    print_as_pointer()
+	// copy/pointer experimentation
+	print_as_copy()
+	print_as_pointer()
 
-    // composition
-    goku := &SaiyanComp{
-      Person: &Person{"Goku"},
-      Power: 9001,
-    }
-    run_noncomposed_introduce( goku )
-    var err bool
-    err = run_composed_introduce_with_errorcheck( goku )
-    fmt.Printf( "err, `%t`\n", err )
+	// composition
+	goku := &SaiyanComp{
+		Person: &Person{"Goku"},
+		Power:  9001,
+	}
+	run_noncomposed_introduce(goku)
+	var err bool
+	err = run_composed_introduce_with_errorcheck(goku)
+	fmt.Printf("err, `%t`\n", err)
 
-}  // end func main()
+} // end func main()
 
-
-/* composition experimentation */
+/* ----------------------------------------------------------------------
+   composition experimentation
+   ---------------------------------------------------------------------- */
 
 type Person struct {
-  Name string
+	Name string
 }
 
 type SaiyanComp struct {
-  *Person
-  Power int
+	*Person
+	Power int
 }
 
 func (p *Person) Introduce() {
-  fmt.Printf("Hi, I'm %s\n", p.Name)
+	fmt.Printf("Hi, I'm %s\n", p.Name)
 }
 
 func (s *SaiyanComp) Introduce() {
-  fmt.Printf("Hi, I'm %s. Ya!\n", s.Name)
+	fmt.Printf("Hi, I'm %s. Ya!\n", s.Name)
 }
 
 func run_noncomposed_introduce(g *SaiyanComp) {
-    g.Introduce()
-    fmt.Println(g.Name)
-    fmt.Println(g.Person.Name)
+	g.Introduce()
+	fmt.Println(g.Name)
+	fmt.Println(g.Person.Name)
 }
 
-func run_composed_introduce_with_errorcheck(g *SaiyanComp) (bool) {
-    g.Person.Introduce()
-    fmt.Println(g.Name)
-    fmt.Println(g.Person.Name)
-    return false
+func run_composed_introduce_with_errorcheck(g *SaiyanComp) bool {
+	g.Person.Introduce()
+	fmt.Println(g.Name)
+	fmt.Println(g.Person.Name)
+	return false
 }
 
-/* --- */
-
-
-/* copy/pointer experimentation */
+/* ----------------------------------------------------------------------
+   copy/pointer experimentation
+   ---------------------------------------------------------------------- */
 
 type Saiyan struct {
-    Name string
-    Power int
+	Name  string
+	Power int
 }
 
 func print_as_copy() {
-    goku := Saiyan{ "Goku", 9000 }
-    SuperCopy( goku )
-    fmt.Printf( "copied power, ```%d```\n", goku.Power )
+	goku := Saiyan{"Goku", 9000}
+	SuperCopy(goku)
+	fmt.Printf("copied power, ```%d```\n", goku.Power)
 }
 
 func print_as_pointer() {
-    goku := &Saiyan{ "Goku", 9000 }
-    SuperPointer( goku )
-    fmt.Printf( "pointered power, ```%d```\n", goku.Power )
+	goku := &Saiyan{"Goku", 9000}
+	SuperPointer(goku)
+	fmt.Printf("pointered power, ```%d```\n", goku.Power)
 }
 
 func SuperCopy(s Saiyan) {
-    s.Power += 10000
+	s.Power += 10000
 }
 
 func SuperPointer(s *Saiyan) {
-    s.Power += 10000
-    // s = &Saiyan{"Gohan", 1000}
-    // fmt.Printf( "s, ```%s```\n", s )
+	s.Power += 10000
+	// s = &Saiyan{"Gohan", 1000}
+	// fmt.Printf( "s, ```%s```\n", s )
 }
 
-/* --- */
+/* ----------------------------------------------------------------------
+   add() experimentation
+   ---------------------------------------------------------------------- */
 
-
-/* add() experimentation */
-
-func add( a int, b int ) int {
-    return a + b
+func add(a int, b int) int {
+	return a + b
 }
 
-/* --- */
+/* ----------------------------------------------------------------------
+   add_and_validate() experimentation
+   ---------------------------------------------------------------------- */
 
-
-/* add_and_validate() experimentation */
-
-func add_and_validate( a int, b int ) ( bool, int ) {
-    return true, a + b
+func add_and_validate(a int, b int) (bool, int) {
+	return true, a + b
 }
 
-/* --- */
-
-
-/* initial struct experimentation */
+/* ----------------------------------------------------------------------
+   initial struct experimentation
+   ---------------------------------------------------------------------- */
 
 func setup_saiyans() {
-    // define the struct
-    type Saiyan struct {
-        Name string
-        Power int
-    }
-    // create an instance
-    var goku Saiyan
-    goku.Name = "Goku"
-    goku.Power = 9000
-    // create another instance
-    birkin := Saiyan{
-        Name: "Birkin",
-        Power: 5000,
-    }
-    fmt.Printf( "goku power, ```%d```\n", goku.Power )
-    fmt.Printf( "birkin power, ```%d```\n", birkin.Power )
+	// define the struct
+	type Saiyan struct {
+		Name  string
+		Power int
+	}
+	// create an instance
+	var goku Saiyan
+	goku.Name = "Goku"
+	goku.Power = 9000
+	// create another instance
+	birkin := Saiyan{
+		Name:  "Birkin",
+		Power: 5000,
+	}
+	fmt.Printf("goku power, ```%d```\n", goku.Power)
+	fmt.Printf("birkin power, ```%d```\n", birkin.Power)
 }
-
-/* --- */
